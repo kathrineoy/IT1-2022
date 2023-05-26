734c5d706a10ce8dd01cfb1f560e5b808eb073ba
kathrineoy <113421131+kathrineoy@users.noreply.github.com>
2023-05-27 00:19:42 +0200

    Add files via upload

---
+++ oppgaver_uke10+11/visitkortJS.js
@@ -0,0 +1,8 @@
+var visittkortInfo = {
+    "personer":[
+        {"navn": "Per Andersen", "epost": "per.andersen@gmail.com", "kjonn":"annet", "nummer":"25468034", "bursdag":"2002-10-06", "foorekort":"nei", "firma":"kiwi", "bilde":"per.jpg"},
+        {"navn": "Kristian Lee", "epost": "kristian.lee@online.no", "kjonn":"mann", "nummer":"99967899", "bursdag":"1997-12-13", "foorekort":"ja", "firma":"XXL", "bilde":"kristianVisitkort.jpg"},
+        {"navn": "Amalie Kongsrud", "epost": "amaliekgr@gmail.com", "kjonn":"kvinne", "nummer":"10739627", "bursdag":"2000-07-20", "foorekort":"ja", "firma":"Follestad dagligleder", "bilde":"amalie.jpg"},
+        {"navn": "Olga Pavlova", "epost": "olgap@pavlovafamily.com", "kjonn":"kvinne", "nummer":"52632041", "bursdag":"1957-01-01", "foorekort":"nei", "firma":"pensjonert", "bilde":"olga.jpg"}
+    ]
+}
